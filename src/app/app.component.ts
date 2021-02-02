b7a183902829a28b9f965e0f9a2dd88f0d1af1ad
HRS0986 <heshanhfernando@gmail.com>
2021-02-02 12:09:29 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'flickr-oauth';
+  constructor(private http: HttpClient) {}
+  getData() {
+    console.log('Getting Data');
+    this.http
+      .get('http://localhost:3000/request')
+      .toPromise()
+      .then((data: any) => {
+        const width = +window.innerWidth - 100;
+        const height = +window.innerHeight - 100;
+        console.log(data);
+        window.location.href = data.data;      
+      })
+      .catch((err) => console.log(err));
+  }
 }
